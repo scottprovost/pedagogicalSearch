07c12b623bf850fbab45a401837074b9d6beb038
sanjuthomas <sanju@sanju.org>
2015-01-23 23:58:53 -0500

    fixing the compile issue

--- src/main/java/org/test/Test.java
+++ src/main/java/org/test/Test.java
@@ -1,5 +1,27 @@
 package org.test;
 
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+
 public class Test {
+
+	public static void main(String[] args) {
+		CrawlConfig crawlConfig = new CrawlConfig();
+		crawlConfig.setCrawlStorageFolder("C:\\asp\\crawler4jStorage");
+		System.out.println(crawlConfig.toString());
+		PageFetcher pageFetcher = new PageFetcher(crawlConfig);
+		RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+		RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig,
+				pageFetcher);
+		try {
+			CrawlController crawlController = new CrawlController(crawlConfig, pageFetcher, robotstxtServer);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 }
--- src/test/java/org/test/TestTest.java
+++ src/test/java/org/test/TestTest.java
@@ -2,15 +2,4 @@ package org.test;
 
 public class TestTest {
 	
-	public static void main(String[] args) {
-        CrawlConfig crawlConfig = new CrawlConfig();
-        crawlConfig.setCrawlStorageFolder("C:\\asp\\crawler4jStorage");
-        System.out.println(crawlConfig.toString());
-        PageFetcher pageFetcher = new PageFetcher(crawlConfig);
-        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
-        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
-        try {
-                CrawlController crawlController = new CrawlController(crawlConfig,
-                        pageFetcher, robotstxtServer);
-
 }
