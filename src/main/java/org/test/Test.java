7c2196a078c3a96b7e2556a240f97fcc16da4fe8
sanjuthomas <sanju@sanju.org>
2015-01-24 11:52:47 -0500

    adding new files

--- src/main/java/org/test/Test.java
+++ src/main/java/org/test/Test.java
@@ -10,7 +10,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		CrawlConfig crawlConfig = new CrawlConfig();
-		crawlConfig.setCrawlStorageFolder("C:\\asp\\crawler4jStorage");
+		crawlConfig.setCrawlStorageFolder("crawler4jStorage");
 		System.out.println(crawlConfig.toString());
 		PageFetcher pageFetcher = new PageFetcher(crawlConfig);
 		RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
@@ -18,6 +18,7 @@ public class Test {
 				pageFetcher);
 		try {
 			CrawlController crawlController = new CrawlController(crawlConfig, pageFetcher, robotstxtServer);
+			crawlController.addSeed("http://www.ics.uci.edu/");
 			
 		} catch (Exception e) {
 			e.printStackTrace();
